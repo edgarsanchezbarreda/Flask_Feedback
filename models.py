2d56d38f110ca58a4a91fce1e5e7ef7ddf0ad35b
Edgar Sanchez <edgar@Edgars-MacBook-Air.local>
2022-05-24 17:09:37 -0700

    completed part 1

---
+++ models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+db = SQLAlchemy()
+
+bcrypt = Bcrypt()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    username = db.Column(
+        db.String(20), 
+        primary_key=True,
+        unique=True,
+        )
+    password = db.Column(
+        db.Text,
+        nullable=False
+        )
+    email = db.Column(
+        db.String(50),
+        nullable=False,
+        unique=True
+        )
+    first_name = db.Column(
+        db.String(30),
+        nullable=False
+        )
+    last_name = db.Column(
+        db.String(30),
+        nullable=False
+        )
