2860bbcaf80a6c5b695bd9e7f11a0930ea582292
Edgar Sanchez <edgar@Edgars-MacBook-Air.local>
2022-05-26 22:38:10 -0700

    still on step 8

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db
-# from forms import 
+from models import connect_db, db, User, Feedback
+from forms import AddFeedbackForm, LoginForm, UserForm, DeleteForm
 from sqlalchemy.exc import IntegrityError
+from werkzeug.exceptions import Unauthorized
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_demo"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "abc123"
@@ -15,3 +16,100 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 connect_db(app)
 
 toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+
+@app.route("/users/<username>")
+def show_user(username):
+    if "username" not in session or username != session['username']:
+        raise Unauthorized()
+
+    user = User.query.get(username)
+    form = DeleteForm()
+    feedback = Feedback.query.all()
+    return render_template('show_user.html', user=user, form=form, feedback=feedback)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_users():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User.register(username, password, email, first_name, last_name)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        session['username'] = new_user.username
+        flash('Welcome! You successfully created your Account', 'success')
+        return redirect(f"/users/{new_user.username}")
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome back, {user.username}", 'info')
+            session['username'] = user.username
+            return redirect(f"/users/{user.username}")
+        else:
+            form.username.errors = ['Invalid username/password!']
+            return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
+
+    
+@app.route('/logout', methods=['GET', 'POST'])
+def logout_user():
+    session.pop('username')
+    flash("You have logged out.", 'info')
+    return redirect('/')
+
+
+@app.route('/users/<username>/delete', methods=['POST'])
+def delete_user(username):
+    user = User.query.get(username)
+    if "username" in session:
+        db.session.delete(user)
+        db.session.commit()
+        session.pop('username')
+        flash("You have deleted your account!", "danger")
+        return redirect('/')
+    return redirect('/')
+
+
+@app.route('/users/<username>/feedback/add', methods=['GET', 'POST'])
+def feedback_add(username):
+    user = User.query.get(username)
+    form = AddFeedbackForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+        usernames = session['username']
+
+        new_feedback = Feedback.add_feedback(title, content, usernames)
+        db.session.add(new_feedback)
+        db.session.commit()
+        flash("You have posted feedback!")
+        return redirect(f"/users/{session['username']}")
+
+    return render_template('/feedback.html', user=user, form=form)
+
+        
--- models.py
+++ models.py
@@ -9,31 +9,44 @@ bcrypt = Bcrypt()
 def connect_db(app):
     db.app = app
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
-
+    db.create_all()
 
 class User(db.Model):
     __tablename__ = 'users'
+    username = db.Column(db.String(20), primary_key=True, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    feedback = db.relationship("Feedback", backref="users", cascade="all,delete")
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate(cls, username, password):
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, password):
+            return u
+        else:
+            return False
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
     username = db.Column(
-        db.String(20), 
-        primary_key=True,
-        unique=True,
-        )
-    password = db.Column(
-        db.Text,
-        nullable=False
-        )
-    email = db.Column(
-        db.String(50),
-        nullable=False,
-        unique=True
-        )
-    first_name = db.Column(
-        db.String(30),
-        nullable=False
-        )
-    last_name = db.Column(
-        db.String(30),
-        nullable=False
-        )
+        db.String(20),
+        db.ForeignKey('users.username'),
+        nullable=False,)
+
+    @classmethod
+    def add_feedback(cls, title, content, username):
+        return cls(title=title, content=content, username=username)
